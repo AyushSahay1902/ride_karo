8bf3cd05fcdb362c4ffa6a77aef52f129cb14d28
Ayush Sahay <sahayayush385@gmail.com>
2024-08-23 14:42:27 +0530

    Project setup deleted

--- app/(root)/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="mt-9 flex flex-col gap-9">
-      <section className="flex flex-col gap-5">
-        <h1 className="text-20 font-bold text-white">Ride Karo</h1>
-      </section>
-    </div>
-  );
-}
